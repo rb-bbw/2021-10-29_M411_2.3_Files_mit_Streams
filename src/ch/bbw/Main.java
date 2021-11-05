ab2abd693ea93f84da3e9922e45146400ec87223
rb-bbw <remo.bergamin@lernende.bbw.ch>
2021-11-05 14:34:24 +0100

    Do task 2.2.1

--- src/ch/bbw/Main.java
+++ src/ch/bbw/Main.java
@@ -3,22 +3,30 @@ package ch.bbw;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
+        task_2_2_1();
+    }
+    private static void task_2_2_1() {
         try (Stream<String> lines = new BufferedReader(
-            new FileReader("resources/wiesel.txt")).lines()) {
+                new FileReader("resources/personen.txt")).lines()) {
 
-            lines.filter(s -> s.length() > 20)
-                .sorted(Comparator.comparingInt(String::length).reversed())
-                .limit(10)
-                .forEach(w -> System.out.printf("%s (%d)%n", w, w.length()));
+            lines.forEach(l -> {
+                String[] values = l.split(";");
+
+                int year = LocalDate.now().getYear();
+                int age = year - Integer.parseInt(values[2]);
+                System.out.println(values[1] + " " + values[0] + " " + age);
+            });
         }
         catch (IOException e) {
             e.printStackTrace();
-        }  // Buffered reader will be closed since Stream implements AutoClosable
+        }
     }
 }
